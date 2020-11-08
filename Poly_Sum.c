94b076eba0040290135687b614a96f43b49f2a83
xkxshs <akashsuresh.sj@gmail.com>
2020-11-08 10:28:07 +0530

    Poly sum Final

--- Poly_Sum.c
+++ Poly_Sum.c
@@ -18,46 +18,52 @@ int main()
         deg3=deg1;
     else
         deg3=deg2;
-    //poly1 reading
-    for (i = 0; i <= deg1; i++)
+    
+    //reading first poly
+    for (i=0 ; i<= deg1 ; i++)
     {
         printf("Enter the coefficeint of x^%d ", i);
         scanf("%d", &a[i].coeff);
         a[i].exp = i;
     }
     printf("\nEnter the coefficient of the second polynomial : \n");
-    //poly2 reading
-    for (i = 0; i <= deg2; i++)
+    
+    //reading second poly
+    for (i=0 ; i<=deg2; i++)
     {
         printf("Enter the coefficeint of x^%d ", i);
         scanf("%d", &b[i].coeff);
         b[i].exp = i;
     }
+
+    //printing first poly
     printf("\nFirst polynomial : ");
-    for(i=0;i<=deg1;i++)
+    for(i=0 ; i<=deg1 ; i++)
     {
         printf("%d", a[i].coeff);
         printf("x^%d ", a[i].exp);
         if(i!=deg1)
             printf("+ ");
     }
+
+    //printing second poly
     printf("\n\nSecond polynomial : ");
-    for(i=0;i<=deg2;i++)
+    for(i=0 ; i<=deg2 ;i++)
     {
         printf("%d", b[i].coeff);
         printf("x^%d ", b[i].exp);
         if(i!=deg2)
             printf("+ ");
     }
-    //poly1 + poly2
+    //Adding
     if (deg1 > deg2)
     {
-        for (i = 0; i <= deg2; i++)
+        for (i=0; i<= deg2 ; i++)
         {
             c[i].coeff = a[i].coeff + b[i].coeff;
             c[i].exp = a[i].exp;
         }
-        for (i = deg2 + 1; i <= deg1; i++)
+        for (i=deg2 + 1 ; i<=deg1 ; i++)
         {
             c[i].coeff = a[i].coeff;
             c[i].exp = a[i].exp;
@@ -66,25 +72,22 @@ int main()
     }
     else
     {
-        for (i = 0; i <= deg1; i++)
+        for (i=0 ; i<=deg1 ; i++)
         {
             c[i].coeff = a[i].coeff + b[i].coeff;
             c[i].exp = b[i].exp;
         }
-        for (i = deg1 + 1; i <= deg2; i++)
+        for (i=deg1 + 1 ; i<=deg2 ; i++)
         {
             c[i].coeff = b[i].coeff;
             c[i].exp = b[i].exp;
         }
-        /*for (i = 0; i < deg2; i++)
-        {
-            printf("%d", a[i].coeff);
-            printf("x^%d ", a[i].exp);
-        }*/
     }
+
+    //printing sum 
     printf("\n\nsum : ");
     
-     for (i = 0; i <= deg3; i++)
+     for (i=0 ; i<=deg3 ; i++)
         {
             printf("%d", c[i].coeff);
             printf("x^%d ", c[i].exp);
